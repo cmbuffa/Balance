8dfb641abbd09ceee246e1c751ed7be61b9ac56f
Carlos Buffa <cmbuffa@gmail.com>
2024-10-07 18:03:45 -0300

    Initial commit

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Balance.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string RemoveWhitespace(this string input)
+        {
+            return new string(input.ToCharArray()
+                .Where(c => !Char.IsWhiteSpace(c))
+                .ToArray());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal balance = 0m;
+            foreach (string[] transaction in ReadRecords())
+            {
+                balance += transaction switch
+                {
+                [_, "DEPOSIT", _, var amount] => decimal.Parse(amount),
+                [_, "WITHDRAWAL", .., var amount] => -decimal.Parse(amount),
+                [_, "INTEREST", var amount] => decimal.Parse(amount),
+                [_, "FEE", var fee] => -decimal.Parse(fee),
+                    _ => throw new InvalidOperationException($"Record {string.Join(", ", transaction)} is not in the expected format!"),
+                };
+                Console.WriteLine($"Record: {string.Join(", ", transaction)}, New balance: {balance:C}");
+            }
+        }
+
+        private static List<string[]> ReadRecords()
+        {
+            var list = new List<string[]>();
+            var records = ReadCsv($"{Environment.CurrentDirectory}/Balance.csv");
+            foreach (var record in records)
+            {
+                var csvRecord = record.Split(',').Select(x => x.Trim()).ToArray();
+                list.Add(csvRecord);
+            }
+
+            return list;
+        }
+
+        private static string[] ReadCsv(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+    }
+}
